377e4ca47486d76581fb10731695d5dbbd163f6f
Walter Nuñez <icarosnet@gmail.com>
2022-10-19 11:08:44 -0500

    a0.0.1
    a0.0.1:
    Alpha Version
    - Docker Structure
    - Project initial structure
    
    know issues:
    - root document not depploy all sub-directory content.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Last Hammer 2
- Last Hammer 2
+Last Hammer 2
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+$files1 = scandir(dirname(__DIR__).'/../');
+
+var_dump($files1);
+
+//require_once dirname(__DIR__).'/../vendor/autoload.php';
